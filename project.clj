e167364cf5dc58ee7e49de9bd371e85217327112
Ludvig Sundström <lud.sund@gmail.com>
2019-02-24 14:07:39 +0100

    Format project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject goal-tracker "0.1.0-SNAPSHOT"
   :description "Personal goal tracking program"
   :url "https://github.com/lsund/goal-tracker"
-
   :min-lein-version "2.7.0"
-
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/core.async  "0.4.474"]
@@ -24,31 +22,27 @@
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [io.aviso/pretty "0.1.34"]]
   :source-paths ["src/clj" "src/cljs"]
-  :ring
-  {:handler goal-tracker.core/new-handler}
-  :main
-  goal-tracker.main
+  :ring {:handler goal-tracker.core/new-handler}
+  :main goal-tracker.main
   :cljsbuild {:builds
               [{:id "dev"
                 :source-paths ["src/cljs"]
                 :figwheel {:on-jsload "goal-tracker.core/on-js-reload"
                            :open-urls ["http://localhost:3449/index.html"]}
-                :compiler {:main                 goal-tracker.core
-                           :asset-path           "js/compiled/out"
-                           :output-to            "resources/public/js/compiled/goal-tracker.js"
-                           :output-dir           "resources/public/js/compiled/out"
+                :compiler {:main goal-tracker.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/goal-tracker.js"
+                           :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true
-                           :preloads             [devtools.preload]}}
+                           :preloads [devtools.preload]}}
                {:id           "min"
                 :source-paths ["src/cljs"]
-                :compiler     {:output-to     "resources/public/js/compiled/goal-tracker.js"
-                               :main          goal-tracker.core
+                :compiler     {:output-to "resources/public/js/compiled/goal-tracker.js"
+                               :main goal-tracker.core
                                :optimizations :advanced
                                :pretty-print  false}}]}
-  :figwheel
-  {:css-dirs ["resources/public/css"]}
-  :repl-options
-  {:init-ns user}
+  :figwheel {:css-dirs ["resources/public/css"]}
+  :repl-options {:init-ns user}
   :profiles {:dev {:dependencies  [[binaryage/devtools "0.9.9"]
                                    [figwheel-sidecar "0.5.15"]
                                    [com.cemerick/piggieback "0.2.2"]]
